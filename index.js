99d990ac5aa0e6616fa043a25944b46da7d7fff6
pussyslain <metajix@gmail.com>
2021-03-07 22:18:34 +0800

    add auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const fs = require("fs");
 const http = require("http");
 const setup = require("proxy");
-// const ngrok = require("ngrok");
+const ngrok = require("ngrok");
 
 async function main() {
   const port = 3128;
@@ -9,6 +10,14 @@ async function main() {
     const port = server.address().port;
     console.log("HTTP(s) proxy server listening on port %d", port);
   });
+  const authtoken = fs.readFileSync("./authtoken", "utf-8");
+  console.log(">>ngrok-termux/index::", "authtoken", authtoken); //TRACE
+  const url = await ngrok.connect({
+    authtoken,
+    proto: "tcp",
+    addr: port,
+  });
+  console.log(">>proxy/index::", "url", url); //TRACE
 }
 
 main();
