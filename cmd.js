a5367dbdf5a864ce8fced1222fec30b3a55c0db7
pussyslain <metajix@gmail.com>
2021-03-15 19:51:38 +0800

    add ssh access

--- cmd.js
+++ cmd.js
@@ -1,6 +1,4 @@
 const { spawn } = require("child_process");
-const fs = require("fs");
-const ngrok = require("ngrok");
 
 async function main() {
   const port = 3244;
@@ -12,16 +10,6 @@ async function main() {
   dir.on("close", (code) =>
     console.log(`child process exited with code ${code}`)
   );
-
-  // const authtoken = fs.readFileSync("./authtoken", "utf-8");
-  const url = await ngrok.connect({
-    // authtoken,
-    proto: "http",
-    addr: port,
-    binPath: () => ".",
-    region: "ap",
-  });
-  console.log(">>proxy/index::", "url", url); //TRACE
 }
 
 main().catch((err) => console.error(err));
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const fs = require("fs");
 const http = require("http");
 const setup = require("proxy");
-const ngrok = require("ngrok");
 
 async function main() {
   const port = 3128;
@@ -10,16 +8,6 @@ async function main() {
     const port = server.address().port;
     console.log("HTTP(s) proxy server listening on port %d", port);
   });
-  // const authtoken = fs.readFileSync("./authtoken", "utf-8");
-  // console.log(">>ngrok-termux/index::", "authtoken", authtoken); //TRACE
-  const url = await ngrok.connect({
-    // authtoken,
-    proto: "tcp",
-    addr: port,
-    binPath: () => ".",
-    region: "ap",
-  });
-  console.log(">>proxy/index::", "url", url); //TRACE
 }
 
 main();
